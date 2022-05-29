2ea84ee0d2b8100ba6a75a172638f1d69bdb1622
Lucas Campos <lucash_c@yahoo.com.br>
2022-05-29 12:54:35 -0300

    melhorias caixa

--- src/components/EditCliente.vue
+++ src/components/EditCliente.vue
@@ -6,6 +6,7 @@
     </q-card-section>
     <div class="q-pa-md">
       <div class="q-gutter-md" style="max-width: 300px">
+
         <q-input outlined v-model="cliente.nome" label="Nome" />
         <q-input outlined v-model="cliente.rg" label="Rg" />
          <q-input outlined v-model="cliente.telefone" label="Telefone" />
--- src/pages/Caixa.vue
+++ src/pages/Caixa.vue
@@ -1,12 +1,15 @@
+/* eslint-disable vue/no-parsing-error */
+/* eslint-disable vue/no-parsing-error */
 <template>
 <q-page class="q-pr-md q-pl-md q-pt-md q-pb-md">
   <div align="center" class="q-pa-md">
    <q-card style="width:250px; height:400px ;">
-   <q-card-section class="bg-primary text-white">
+   <q-card-section class="bg-primary text-white" >
       <div class="text-h6 q-pl-md">Caixa</div>
     </q-card-section>
-      <q-card-section>
-        <div align="center">
+      <q-card-section style=height:280px>
+          <q-input v-if="!open" style="margin-top: 70px" outlined label="Codigo do Ticket"/>
+        <div  v-if="open" align="center">
           <q-badge color="blue">
           Numero: {{ticket.numero}}
          </q-badge>
@@ -24,42 +27,73 @@
          </q-badge>
           <br>
           <q-badge color="blue">
-          Valor: {{ticket.valor}}
+          Valor: R${{formatCurrency(ticket.valor)}}
          </q-badge>
          <br>
-        <br>
-        <q-input outlined v-model="ticket.valorPago" label="Valor Pago" />
+        <q-input v-if="newTicket.tipoPagamento == 'Dinheiro'" style="padding:10px" dense outlined v-model="newTicket.valorPago" label="Valor Pago R$"/>
+        <q-select style="padding:10px" dense outlined options-dense :options="['Cartão', 'Dinheiro']" v-model="newTicket.tipoPagamento" label="Tipo de pagamento" />
+        <span v-if="newTicket.tipoPagamento == 'Dinheiro'"> {{ (newTicket.valorPago - ticket.valor) >= 0 ? 'Troco: R$'+formatCurrency(newTicket.valorPago - ticket.valor) : 'Digite o valor pago'}}</span>
         </div>
       </q-card-section>
-    </q-card>
+    <!-- footer----------------------------------------------------------------------->
+    <q-card-actions align="center">
+      <div>
+         <q-btn
+        :disable="newTicket.tipoPagamento == 'Dinheiro' && (newTicket.valorPago - ticket.valor) < 0"
+          :label="open?'Cobrar': 'Prosseguir'"
+          type="submit"
+          color="primary"
+          @click="open?save():prosseguir()"
+        >
+        </q-btn>
+      </div>
+    </q-card-actions>
+  </q-card>
   </div>
   </q-page>
 </template>
 
 <script>
-import { defineComponent } from 'vue'
+import { defineComponent, ref } from 'vue'
+import { formatCurrency } from 'src/util/format'
 const ticket =
   {
     numero: '546645465',
     dhEntrada: '25/04/2022 11:15:12',
     dhSaida: '25/04/2022 11:15:12',
     tipo: 'comum',
-    valor: 25
+    valor: 30
   }
+
 export default defineComponent({
   name: 'Movimento',
 
-  components: {
+  methods: {
+    prosseguir () {
+      this.open = true
+    },
 
+    openTicket () {
+      this.newTicket = this.ticket
+      this.newTicket = {
+        tipoPagamento: 'Dinheiro',
+        valorPago: 0
+      }
+      this.open = true
+    }
   },
+
   data () {
     return {
+      open: ref(false),
+      newTicket: {}
     }
   },
 
   setup () {
     return {
-      ticket
+      ticket,
+      formatCurrency
     }
   }
 })
